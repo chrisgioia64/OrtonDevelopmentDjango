0e4815e5bb391afbaa1e2f15ebb09baf93dfc2ab
Christopher Gioia <christophergioia64@gmail.com>
2021-09-24 12:57:11 -0700

    update models

--- webapp/settings.py
+++ webapp/settings.py
@@ -114,6 +114,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = 'media/'
+
+MEDIA_URL = 'media/'
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
